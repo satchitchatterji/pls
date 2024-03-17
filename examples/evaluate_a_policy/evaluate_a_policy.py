99d7804cfa2c7947834f7cab6d8ebc5edb738fc9
Satchit Chatterji <satchit.chatterji@gmail.com>
2024-03-17 12:56:13 +0100

    Verified pacman working without shield

--- examples/evaluate_a_policy/evaluate_a_policy.py
+++ examples/evaluate_a_policy/evaluate_a_policy.py
@@ -4,7 +4,7 @@ import os
 if __name__ == "__main__":
     # current working directory
     cwd = os.path.join(os.path.dirname(__file__))
-    config_file = os.path.join(cwd, "../train_a_policy/carracing/no_shield/seed1/config.json")
+    config_file = os.path.join(cwd, "../train_a_policy/pacman/no_shield/seed1/config.json")
     print(f"config_file: {config_file}")
 
     mean_reward, std_reward = evaluate(config_file, model_at_step="end", n_test_episodes=10)
